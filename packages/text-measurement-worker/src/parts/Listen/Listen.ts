0c0adf73e3071b37ce2ea09356419ff5bacdd97e
Le Vivilet <levivilet@pm.me>
2025-11-19 19:06:11 +0100

    feature: set rpc

--- packages/text-measurement-worker/src/parts/Listen/Listen.ts
+++ packages/text-measurement-worker/src/parts/Listen/Listen.ts
@@ -1,8 +1,10 @@
 import { WebWorkerRpcClient } from '@lvce-editor/rpc'
+import { EditorWorker } from '@lvce-editor/rpc-registry'
 import * as CommandMap from '../CommandMap/CommandMap.ts'
 
 export const listen = async (): Promise<void> => {
-  await WebWorkerRpcClient.create({
+  const rpc = await WebWorkerRpcClient.create({
     commandMap: CommandMap.commandMap,
   })
+  EditorWorker.set(rpc)
 }
