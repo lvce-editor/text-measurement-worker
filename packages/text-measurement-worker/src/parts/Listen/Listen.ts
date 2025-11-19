109dff4612131c61beb7bed6dbf4a7fde90a4de0
Le Vivilet <levivilet@pm.me>
2025-11-20 00:06:44 +0100

    feature: add handleMessagePort

--- packages/text-measurement-worker/src/parts/Listen/Listen.ts
+++ packages/text-measurement-worker/src/parts/Listen/Listen.ts
@@ -1,8 +1,10 @@
 import { WebWorkerRpcClient } from '@lvce-editor/rpc'
 import { EditorWorker } from '@lvce-editor/rpc-registry'
 import * as CommandMap from '../CommandMap/CommandMap.ts'
+import * as CommandMapRef from '../CommandMapRef/CommandMapRef.ts'
 
 export const listen = async (): Promise<void> => {
+  Object.assign(CommandMapRef.commandMapRef, CommandMap.commandMap)
   const rpc = await WebWorkerRpcClient.create({
     commandMap: CommandMap.commandMap,
   })
