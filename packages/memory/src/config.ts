09a683583a67da7745ea4d285ea774d86d7a4f35
Le Vivilet <levivilet@pm.me>
2025-11-19 20:58:59 +0100

    fix

--- packages/memory/src/config.ts
+++ packages/memory/src/config.ts
@@ -7,6 +7,6 @@ export const workerPath = join(root, '.tmp/dist/dist/textMeasurementWorkerMain.j
 
 export const playwrightPath = new URL('../../e2e/node_modules/playwright/index.mjs', import.meta.url).toString()
 
-export const instantiations = 5000
+export const instantiations = 8000
 
 export const instantiationsPath = join(root, 'packages', 'text-measurement-worker')
