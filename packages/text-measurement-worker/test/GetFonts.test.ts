a4c487f54dba32fc549b5248d70a48c65009349e
Le Vivilet <levivilet@pm.me>
2025-11-19 22:27:55 +0100

    simplify

--- packages/text-measurement-worker/test/GetFonts.test.ts
+++ packages/text-measurement-worker/test/GetFonts.test.ts
@@ -3,28 +3,22 @@ import * as GetFonts from '../src/parts/GetFonts/GetFonts.ts'
 import { mockFonts } from '../src/parts/MockFonts/MockFonts.ts'
 
 test('getFonts returns globalThis.fonts when available', () => {
-  mockFonts()
   const mockFontFaceSet = {
     add: () => {},
   } as unknown as FontFaceSet
 
-  // @ts-ignore
-  globalThis.fonts = mockFontFaceSet
+  mockFonts({ fonts: mockFontFaceSet })
 
   const result = GetFonts.getFonts()
   expect(result).toBe(mockFontFaceSet)
 })
 
 test('getFonts returns document.fonts when globalThis.fonts is not available', () => {
-  mockFonts({ mockDocument: true })
-  // @ts-ignore
-  delete globalThis.fonts
   const mockFontFaceSet = {
     add: () => {},
   } as unknown as FontFaceSet
 
-  // @ts-ignore
-  document.fonts = mockFontFaceSet
+  mockFonts({ fonts: mockFontFaceSet, mockDocument: true, useGlobalFonts: false })
 
   const result = GetFonts.getFonts()
   expect(result).toBe(mockFontFaceSet)
