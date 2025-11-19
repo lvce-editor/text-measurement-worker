aae2068caa4c72d75357ab186fdb6ba06c29a7d0
Le Vivilet <levivilet@pm.me>
2025-11-19 19:18:45 +0100

    feature: add letterSpacing test

---
+++ packages/text-measurement-worker/test/GetLetterSpacingString.test.ts
@@ -0,0 +1,9 @@
+import { expect, test } from '@jest/globals'
+import * as GetLetterSpacingString from '../src/parts/GetLetterSpacingString/GetLetterSpacingString.ts'
+
+test('getLetterSpacingString', () => {
+  expect(GetLetterSpacingString.getLetterSpacingString(0)).toBe('0px')
+  expect(GetLetterSpacingString.getLetterSpacingString(1)).toBe('1px')
+  expect(GetLetterSpacingString.getLetterSpacingString(1.5)).toBe('1.5px')
+  expect(GetLetterSpacingString.getLetterSpacingString(-0.5)).toBe('-0.5px')
+})
