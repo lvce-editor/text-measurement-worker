b2275beb82b117a0202e6683b37eb38d0d7ba230
Le Vivilet <levivilet@pm.me>
2025-11-19 19:01:34 +0100

    feature: register font command (#10)

--- packages/text-measurement-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/text-measurement-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,5 +1,7 @@
+import * as Font from '../Font/Font.ts'
 import * as MeasureTextWidth from '../MeasureTextWidth/MeasureTextWidth.ts'
 
 export const commandMap = {
   'TextMeasurement.measureTextWidth': MeasureTextWidth.measureTextWidth,
+  'TextMeasurement.ensureFont': Font.ensure,
 }
