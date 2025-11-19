d0e9a944901c198a44b73136597357f50af8a151
Le Vivilet <levivilet@pm.me>
2025-11-19 19:00:27 +0100

    lint

--- packages/text-measurement-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/text-measurement-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,5 +1,5 @@
-import * as MeasureTextWidth from '../MeasureTextWidth/MeasureTextWidth.ts'
 import * as Font from '../Font/Font.ts'
+import * as MeasureTextWidth from '../MeasureTextWidth/MeasureTextWidth.ts'
 
 export const commandMap = {
   'TextMeasurement.measureTextWidth': MeasureTextWidth.measureTextWidth,
