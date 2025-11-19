152919f32c11a30ff7ef64519744310568fae050
Le Vivilet <levivilet@pm.me>
2025-11-19 22:53:59 +0100

    feature: use class for textSegmenter (#60)

--- packages/text-measurement-worker/src/parts/TextSegmenter/TextSegmenter.ts
+++ packages/text-measurement-worker/src/parts/TextSegmenter/TextSegmenter.ts
@@ -5,41 +5,51 @@ export interface TextSegmenterAdapter {
   readonly getSegments: (line: string) => Intl.Segments
 }
 
+export class TextSegmenterAdapterClass implements TextSegmenterAdapter {
+  private readonly segmenter: Intl.Segmenter
+
+  constructor() {
+    this.segmenter = new Intl.Segmenter()
+  }
+
+  at(line: string, index: number): Intl.SegmentData | undefined {
+    const segments = this.segmenter.segment(line)
+    return segments.containing(index)
+  }
+
+  visualIndex(line: string, index: number): number {
+    const segments = this.segmenter.segment(line)
+    let currentVisualIndex = 0
+    for (const segment of segments) {
+      if (segment.index >= index) {
+        return currentVisualIndex
+      }
+      currentVisualIndex++
+    }
+    return currentVisualIndex
+  }
+
+  modelIndex(line: string, visualIndex: number): number {
+    const segments = this.segmenter.segment(line)
+    let currentVisualIndex = 0
+    for (const segment of segments) {
+      if (currentVisualIndex >= visualIndex) {
+        return segment.index
+      }
+      currentVisualIndex++
+    }
+    return line.length
+  }
+
+  getSegments(line: string): Intl.Segments {
+    return this.segmenter.segment(line)
+  }
+}
+
 export const supported = (): boolean => {
   return 'Segmenter' in Intl
 }
 
 export const create = (): TextSegmenterAdapter => {
-  const segmenter = new Intl.Segmenter()
-  return {
-    at(line: string, index: number): Intl.SegmentData | undefined {
-      const segments = segmenter.segment(line)
-      return segments.containing(index)
-    },
-    visualIndex(line: string, index: number): number {
-      const segments = segmenter.segment(line)
-      let currentVisualIndex = 0
-      for (const segment of segments) {
-        if (segment.index >= index) {
-          return currentVisualIndex
-        }
-        currentVisualIndex++
-      }
-      return currentVisualIndex
-    },
-    modelIndex(line: string, visualIndex: number): number {
-      const segments = segmenter.segment(line)
-      let currentVisualIndex = 0
-      for (const segment of segments) {
-        if (currentVisualIndex >= visualIndex) {
-          return segment.index
-        }
-        currentVisualIndex++
-      }
-      return line.length
-    },
-    getSegments(line: string): Intl.Segments {
-      return segmenter.segment(line)
-    },
-  }
+  return new TextSegmenterAdapterClass()
 }
