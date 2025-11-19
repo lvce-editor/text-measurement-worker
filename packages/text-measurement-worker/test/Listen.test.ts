a0865b7f3e54d4a73866309669f1f7cb62d9dd8a
Le Vivilet <levivilet@pm.me>
2025-11-19 21:26:55 +0100

    feature: enable listen test

--- packages/text-measurement-worker/test/Listen.test.ts
+++ packages/text-measurement-worker/test/Listen.test.ts
@@ -1,13 +1,8 @@
-import { beforeEach, test } from '@jest/globals'
+import { test } from '@jest/globals'
 import { mockWorkerGlobalRpc } from '@lvce-editor/rpc'
-import { mockOffscreenCanvas } from '../src/mockOffscreenCanvas.ts'
 import { listen } from '../src/parts/Listen/Listen.ts'
 
-beforeEach(() => {
-  mockOffscreenCanvas()
-})
-
-test.skip('listen', async () => {
+test('listen', async () => {
   const { start, dispose } = mockWorkerGlobalRpc()
   const listenPromise = listen()
   start()
--- packages/text-measurement-worker/test/Main.test.ts
+++ packages/text-measurement-worker/test/Main.test.ts
@@ -1,13 +1,8 @@
-import { beforeEach, test } from '@jest/globals'
+import { test } from '@jest/globals'
 import { mockWorkerGlobalRpc } from '@lvce-editor/rpc'
-import { mockOffscreenCanvas } from '../src/mockOffscreenCanvas.ts'
 import { main } from '../src/parts/Main/Main.ts'
 
-beforeEach(() => {
-  mockOffscreenCanvas()
-})
-
-test.skip('main', async () => {
+test('main', async () => {
   const { start, dispose } = mockWorkerGlobalRpc()
   const mainPromise = main()
   start()
