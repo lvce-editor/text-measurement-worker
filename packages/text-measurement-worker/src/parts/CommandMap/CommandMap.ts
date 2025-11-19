05f991b56bb93e48f0f8fcb0349cf17edffd1136
Le Vivilet <levivilet@pm.me>
2025-11-19 21:34:21 +0100

    feature: register unicode command

--- packages/text-measurement-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/text-measurement-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,7 +1,9 @@
 import * as Font from '../Font/Font.ts'
+import * as GetAccurateColumnIndexUnicode from '../GetAccurateColumnIndexUnicode/GetAccurateColumnIndexUnicode.ts'
 import * as MeasureTextWidth from '../MeasureTextWidth/MeasureTextWidth.ts'
 
 export const commandMap = {
   'TextMeasurement.measureTextWidth': MeasureTextWidth.measureTextWidth,
   'TextMeasurement.ensureFont': Font.ensure,
+  'TextMeasurement.getAccurateColumnIndexUnicode': GetAccurateColumnIndexUnicode.getAccurateColumnIndexUnicode,
 }
