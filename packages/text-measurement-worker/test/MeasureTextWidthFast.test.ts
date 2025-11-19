510e902d845f8419e8240beb33142207d44d0411
Le Vivilet <levivilet@pm.me>
2025-11-19 19:19:16 +0100

    feature: add measureTextWidth test (#23)

---
+++ packages/text-measurement-worker/test/MeasureTextWidthFast.test.ts
@@ -0,0 +1,26 @@
+import { expect, test } from '@jest/globals'
+import * as MeasureTextWidthFast from '../src/parts/MeasureTextWidthFast/MeasureTextWidthFast.ts'
+
+test('measureTextWidthFast with single character', () => {
+  expect(MeasureTextWidthFast.measureTextWidthFast('a', 10)).toBe(10)
+})
+
+test('measureTextWidthFast with multiple characters', () => {
+  expect(MeasureTextWidthFast.measureTextWidthFast('abc', 10)).toBe(30)
+})
+
+test('measureTextWidthFast with empty string', () => {
+  expect(MeasureTextWidthFast.measureTextWidthFast('', 10)).toBe(0)
+})
+
+test('measureTextWidthFast with different charWidth', () => {
+  expect(MeasureTextWidthFast.measureTextWidthFast('hello', 8)).toBe(40)
+})
+
+test('measureTextWidthFast with decimal charWidth', () => {
+  expect(MeasureTextWidthFast.measureTextWidthFast('ab', 7.5)).toBe(15)
+})
+
+test('measureTextWidthFast with unicode characters', () => {
+  expect(MeasureTextWidthFast.measureTextWidthFast('你好', 10)).toBe(20)
+})
