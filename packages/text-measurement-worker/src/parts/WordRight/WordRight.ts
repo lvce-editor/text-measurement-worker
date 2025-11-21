7531a71c967cfd07b6c873762a6393b6e304ef51
Le Vivilet <levivilet@pm.me>
2025-11-21 19:41:30 +0100

    feature: improve matching for french accent

--- packages/text-measurement-worker/src/parts/WordRight/WordRight.ts
+++ packages/text-measurement-worker/src/parts/WordRight/WordRight.ts
@@ -1,14 +1,14 @@
-import * as T from '../TextSegmenter/TextSegmenter.ts'
+import * as TextSegmenter from '../TextSegmenter/TextSegmenter.ts'
 
 const wordSeparators = ['.', '/', '?', '\\', '[', ']', '{', '}', '$', '%', '&', '<', '>', '|']
 
 export const wordRight = (text: string): number => {
-  const segmenter = T.create({ granularity: 'word' })
+  const segmenter = TextSegmenter.create({ granularity: 'word', localeMatcher: 'best fit' })
   const segments = segmenter.getSegments(text)
   for (const segment of segments) {
     if (wordSeparators.includes(segment.segment)) {
       return segment.index
     }
   }
-  return 1
+  return text.length
 }
