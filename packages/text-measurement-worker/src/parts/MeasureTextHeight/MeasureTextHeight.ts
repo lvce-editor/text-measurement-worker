63c0c9cadbb9464968213387cbdc2c1c38b7c44b
Le Vivilet <levivilet@pm.me>
2026-05-28 01:07:58 +0200

    feature: add TODO comments for text block height measurement implementation (#95)

--- packages/text-measurement-worker/src/parts/MeasureTextHeight/MeasureTextHeight.ts
+++ packages/text-measurement-worker/src/parts/MeasureTextHeight/MeasureTextHeight.ts
@@ -13,6 +13,12 @@ export const measureTextBlockHeight = async (
   lineHeight: number | string,
   width: number,
 ): Promise<number> => {
+  // TODO 1. create canvas
+  // 2. create ctx
+  // 3. check if whole text fits into available width
+  // 4. if not, split text into words
+  // 5. measure each words length
+  // 6. do a simple layout algorithm to determine how many lines we need
   // @ts-ignore
   // return RendererProcess.invoke('MeasureTextBlockHeight.measureTextBlockHeight', text, fontSize, fontFamily, lineHeight, width)
   return 100
