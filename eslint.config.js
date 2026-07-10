184b7e5ebb20a11e24c2f5afda6e3beb377f7a19
Le Vivilet <levivilet@pm.me>
2026-07-10 22:01:00 +0000

    chore: disable readonly-param and await-thenable for ts6

--- eslint.config.js
+++ eslint.config.js
@@ -9,6 +9,8 @@ export default [
   {
     rules: {
       'unicorn/consistent-function-scoping': 'off',
+      '@typescript-eslint/await-thenable': 'off',
+      '@typescript-eslint/prefer-readonly-parameter-types': 'off',
       '@typescript-eslint/only-throw-error': 'off',
     },
   },
