b76bc914487f1423064411513f40e53a304bec15
Le Vivilet <levivilet@pm.me>
2026-08-02 02:34:01 +0000

    feature: update eslint dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,10 @@
+import { defineConfig } from 'eslint/config'
 import * as config from '@lvce-editor/eslint-config'
-import * as actions from '@lvce-editor/eslint-plugin-github-actions'
 import * as tsconfig from '@lvce-editor/eslint-plugin-tsconfig'
 
-export default [
+export default defineConfig([
   ...config.default,
-  ...actions.default,
+  ...config.recommendedActions,
   ...tsconfig.default,
   {
     rules: {
@@ -31,4 +31,4 @@ export default [
       'unicorn/prefer-number-coercion': 'off',
     },
   },
-]
+])
